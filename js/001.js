6772b77b95dcfce16fba7d4117ca421b01eb7414
vlobo <valentinlobo@hotmail.com>
2024-07-06 23:43:53 -0300

    terminado ejercicio 1

--- js/001.js
+++ js/001.js
@@ -1,2 +1,6 @@
-//1. - Escribe un programa de una sola línea que haga que aparezca en la pantalla un alert que diga “un mensaje”.
+// 1.- Escribe un programa de una sola línea que haga que aparezca en la pantalla un alert que diga “un mensaje”.
 
+
+
+
+alert("ejercicio n1")

d90667189595af911312fa8986ee733a6186cb7b
vlobo <valentinlobo@hotmail.com>
2024-07-09 00:11:17 -0300

    terminado el ejercicio 3 de sumar 2 variables

---
+++ js/003.js
@@ -0,0 +1,16 @@
+
+
+// 3.- Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
+
+
+let numeroA = 5
+let numeroB = 8
+
+var suma = numeroA + numeroB
+
+
+console.log(suma);
+
+document.write("la suma es igual = "+ suma + "");
+
+

527bf516c931f7ed628203976d987354823541b2
vlobo <valentinlobo@hotmail.com>
2024-07-14 09:43:40 -0300

    se crea el archivo readme

---
+++ js/005.js
@@ -0,0 +1,7 @@
+/*
+
+5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+Nota: Tener en cuenta la siguiente función: parseInt
+
+*/
+
